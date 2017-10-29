0ef7d541bfdcda50a756738b7ffd67de646bbe04
yiannis13 <yiannis.ioannidis@hotmail.gr>
2017-10-29 17:29:03 +0100

    Set up the project. Create a basic Controller, change a new bootstrap.css file and small code format.

---
+++ ViFlix/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using ViFlix.Models;
+
+namespace ViFlix.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Random()
+        {
+            var movie = new Movie
+            {
+                Id = 1,
+                Name = "Seven"
+            };
+
+            return View(movie);
+        }
+    }
+}
