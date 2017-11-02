97f1dc41d60d99d7b47d188dd3e3f094fbfe4d20
yiannis13 <yiannis.ioannidis@hotmail.gr>
2017-11-02 12:42:14 +0100

    Include bootbox JS library for making delete confirmation pop up look better. Also create a new JS file to include all our scripts.

---
+++ ViFlix/Scripts/myScripts.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $("#customers").on("click", ".js-del",
+        function () {
+            var button = $(this);
+
+            bootbox.confirm("Are you sure you want to delete this customer?", function(confirmed) {
+                if (confirmed) {
+                    $.ajax({
+                        url: "/api/customers/" + button.attr("customer-id"),
+                        method: "DELETE",
+                        success: function () {
+                            button.parents("tr").remove();
+                        }
+                    });
+                }
+            });
+        });
+});
