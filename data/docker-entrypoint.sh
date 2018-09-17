551be6213096fbbc01b01a9c22939fca91ce4601
Yiannis_Ioannidis <ioannis.ioannidis@ac-project.net>
2018-09-17 20:20:52 +0200

    Fix entrypoint error.

--- data/docker-entrypoint.sh
+++ data/docker-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
-
 echo "The Dockerfile ENTRYPOINT started. . ."
-
-#/opt/mssql-tools/bin/sqlcmd -U sa -P Passw0rd! -S viflix_sqlserver_1 
-#/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Passw0rd!" -i migration.sql
-
+/opt/mssql-tools/bin/sqlcmd -S "localhost" -U "sa" -P "Passw0rd!" -i "migration.sql"
 echo "The Dockerfile ENTRYPOINT is executing. . ."
+exec "$@"
