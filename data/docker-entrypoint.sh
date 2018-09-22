f7a1f0e728c6dd501b26f29dd8f36165f83fcde0
yiannis13 <yiannis.ioannidis@hotmail.gr>
2018-09-22 15:58:54 +0200

    target net472 instead of net461.

--- data/docker-entrypoint.sh
+++ data/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
-echo "The Dockerfile ENTRYPOINT started. . ."
-/opt/mssql-tools/bin/sqlcmd -S "localhost" -U "sa" -P "Passw0rd!" -i "migration.sql"
-echo "The Dockerfile ENTRYPOINT is executing. . ."
+echo "The Dockerfile ENTRYPOINT is starting. . ."
+sleep 1s
+/opt/mssql-tools/bin/sqlcmd -S "localhost" -U "sa" -P "Passw0rd!" -i "migration.sql" \
+& /opt/mssql/bin/sqlservr 
 exec "$@"
